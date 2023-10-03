a955b45025d7fdb4493b8c0c6366f8bcc33fa9d7
LeeYulhee <reasonoflife39@gmail.com>
2023-10-04 02:46:10 +0900

    feat : session에 저장한 값을 새로고침 후에도 보이게 하기

--- src/main/java/us/yhee/releasing/domain/holisticReleasing/controller/HolisticReleasingApiController.java
+++ src/main/java/us/yhee/releasing/domain/holisticReleasing/controller/HolisticReleasingApiController.java
@@ -2,6 +2,7 @@ package us.yhee.releasing.domain.holisticReleasing.controller;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +16,14 @@ public class HolisticReleasingApiController {
 
     @GetMapping("/api/start")
     public Map<String, Object> start(HttpSession session, @RequestParam String subject) {
+
+        String question = String.format("나는 최대한 %s을 환영하도록 나를 허용할 수 있나요?", subject);
+
         session.setAttribute("subject", subject);
         session.setAttribute("count", 0);
         session.setAttribute("mode", "resist");
+        session.setAttribute("question", question);
 
-        String question = String.format("나는 최대한 %s을 환영하도록 나를 허용할 수 있나요?", subject);
         Map<String, Object> response = new HashMap<>();
         response.put("question", question);
         response.put("count", 0);
@@ -38,21 +42,29 @@ public class HolisticReleasingApiController {
 
         if(mode.equals("welcome")) {
             question = String.format("나는 최대한 %s을 환영하도록 나를 허용할 수 있나요?", subject);
-        } else {
-            question = String.format("나는 최대한 %s에 저항하도록 나를 허용할 수 있나요?", subject);
-        }
-
-        if ("welcome".equals(mode)) {
             session.setAttribute("count", ++count);
             session.setAttribute("mode", "resist");
         } else {
+            question = String.format("나는 최대한 %s에 저항하도록 나를 허용할 수 있나요?", subject);
             session.setAttribute("mode", "welcome");
         }
+
+        session.setAttribute("question", question);
 
         Map<String, Object> response = new HashMap<>();
         response.put("question", question);
         response.put("count", count);
 
         return response;
+    }
+
+    @GetMapping("/api/reload")
+    public ResponseEntity<Map<String, Object>> reload(HttpSession session) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("subject", session.getAttribute("subject"));
+        response.put("question", session.getAttribute("question"));
+        response.put("count", session.getAttribute("count"));
+
+        return ResponseEntity.ok(response);
     }
 }
