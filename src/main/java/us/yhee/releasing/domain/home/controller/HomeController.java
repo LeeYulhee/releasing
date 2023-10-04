b2db567202c6ee7d1596bb94639155d5a8f25b3a
LeeYulhee <reasonoflife39@gmail.com>
2023-10-05 00:41:17 +0900

    design : 홈화면 추가

--- src/main/java/us/yhee/releasing/domain/home/controller/HomeController.java
+++ src/main/java/us/yhee/releasing/domain/home/controller/HomeController.java
@@ -7,7 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
+    public String start() {
+        return "redirect:/home";
+    }
+
+    @GetMapping("/home")
     public String home() {
-        return "redirect:/releasing/holistic";
+        return "home/home";
     }
 }
